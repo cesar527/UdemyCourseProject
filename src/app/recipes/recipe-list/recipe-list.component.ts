621e579ea63096a12d6edeb6b15854dadd1a0e2d
César Aprea Castro <cesar.aprea@itc-engineering.com>
2018-08-17 23:12:27 +0200

    #course

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,14 +7,18 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
   new Recipe('A Test Recipe', 'This simply a test', 'http://estaticos.telva.com/apps/cocina/imagenes/recetas/carne/jamon_cocido.jpg'),
     new Recipe('A Test Recipe 2', 'This simply a test 2', 'http://estaticos.telva.com/apps/cocina/imagenes/recetas/carne/jamon_cocido.jpg')
   ];
-
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
